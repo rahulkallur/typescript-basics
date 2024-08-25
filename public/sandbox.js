d547e978f182d453b9f0f3130f23bd674e0d005b
rahulkallur <rahulkallur63@gmail.com>
2024-08-25 18:47:49 +0530

    DOM & Type casting

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("test");
+// const anchor = document.querySelector('a');
+// console.log(anchor)
+const form = document.querySelector(".new-item-form");
+console.log(form.children);
+// inputs
+const type = document.querySelector("#type");
+const toForm = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, toForm.value, details.value, amount.valueAsNumber);
+});
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log("test")
+// const anchor = document.querySelector('a');
+
+// console.log(anchor)
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+console.log(form.children);
+
+// inputs
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toForm = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, toForm.value, details.value, amount.valueAsNumber);
+});
