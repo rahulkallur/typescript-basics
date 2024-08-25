b512800d3307123a9272013a98019ab59e70bd8c
rahulkallur <rahulkallur63@gmail.com>
2024-08-25 18:29:39 +0530

    function signature

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,26 @@
-console.log("test")
+// Exmaple1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, message: string) => {
+  console.log(`${name} says ${message}`);
+};
+
+// Example2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (a: number, b: number, action: string) => {
+  if (action === "add") {
+    return a * b;
+  } else {
+    return a - b;
+  }
+};
+
+// Example3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type Person = { name: string; age: number };
+
+logDetails = (obj: Person) => {
+  console.log(`${obj.name} is ${obj.age} years old`);
+};
