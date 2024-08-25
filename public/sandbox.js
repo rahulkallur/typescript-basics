4e1061736c6ee7bdd20761e6525f45cb82f77c5c
rahulkallur <rahulkallur63@gmail.com>
2024-08-25 13:16:13 +0530

    functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("test");
+let greet;
+greet = () => {
+    console.log("Hello");
+};
+const add = (a, b, c = 15) => {
+    console.log(a + b);
+};
+add(10, 5);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 5);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-console.log("test")
+let greet: Function;
+
+greet = () => {
+  console.log("Hello");
+};
+
+const add = (a: number, b: number, c: number | string = 15) => {
+  console.log(a + b);
+};
+
+add(10, 5);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 5);
+console.log(result);
