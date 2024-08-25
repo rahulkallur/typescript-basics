93ff3751507deb69678929a98be30dba3006c9c8
rahulkallur <rahulkallur63@gmail.com>
2024-08-25 13:23:49 +0530

    type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("test");
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+const greeting = (user) => {
+    console.log(`${user.name} say hello`);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,13 @@
-console.log("test")
+type StringOrNum = string | number;
+type ObjWithName = {
+  name: string;
+  uid: StringOrNum;
+};
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
+
+const greeting = (user: ObjWithName) => {
+  console.log(`${user.name} say hello`);
+};
