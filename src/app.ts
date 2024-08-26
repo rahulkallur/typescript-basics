4f3e224f19481cdaa192e957964ee742b6149b54
rahulkallur <rahulkallur63@gmail.com>
2024-08-26 09:57:26 +0530

    intefaces demo

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,50 @@
-import { Invoice } from './classes/Invoice.js';
+interface isPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+const me: isPerson = {
+  name: "John",
+  age: 29,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    return amount;
+  },
+};
+
+console.log(me,"me")
+
+import { Invoice } from "./classes/Invoice.js";
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
 // invOne.client = 'yoshi';
 // invOne.amount = 50;
 
 let invoices: Invoice[] = [];
-invoices.push(invOne)
+invoices.push(invOne);
 invoices.push(invTwo);
 
-invoices.forEach(inv => {
+invoices.forEach((inv) => {
   console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-})
+});
 
-
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
